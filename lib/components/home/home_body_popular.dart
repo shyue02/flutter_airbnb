3192b02bfb249f5ac22bbdbb0aefb977e33daaa7
shyue02 <shyue02@naver.com>
2022-11-14 16:56:49 +0900

    완성

--- lib/components/home/home_body_popular.dart
+++ lib/components/home/home_body_popular.dart
@@ -1,28 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_airbnb/components/home/home_body_popular_item.dart';
+import 'package:flutter_airbnb/size.dart';
+import 'package:flutter_airbnb/styles.dart';
 
 class HomeBodyPopular extends StatelessWidget {
   const HomeBodyPopular({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽정렬
-      children: [
-        _buildPopularTitle(),
-        _buildPopularList(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(top: gap_m),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽정렬
+        children: [
+          _buildPopularTitle(),
+          _buildPopularList(),
+        ],
+      ),
     );
   }
 
   Widget _buildPopularTitle() {
-    return SizedBox();
+    return Column(
+      children: [
+        Text(
+          "한국 속소에 직접 다녀간 게스트의 후기",
+          style: h5(),
+        ),
+        Text(
+          "게스트 후기 2,500,000개 이상, 평균 평점 4.7점(5점 만점)",
+          style: body1(),
+        ),
+        SizedBox(height: gap_m),
+      ],
+    );
   }
 
   Widget _buildPopularList() {
     // 나중에 ROW로 변경해보기
     return Container(
-      color: Colors.red,
       child: Wrap(
         children: [
           HomeBodyPopularItem(id: 0),
--- lib/components/home/home_body_popular_item.dart
+++ lib/components/home/home_body_popular_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_airbnb/constants.dart';
 import 'package:flutter_airbnb/size.dart';
+import 'package:flutter_airbnb/styles.dart';
 
 class HomeBodyPopularItem extends StatelessWidget {
   HomeBodyPopularItem({required this.id, Key? key}) : super(key: key);
@@ -18,8 +19,6 @@ class HomeBodyPopularItem extends StatelessWidget {
     double popularItemWidth = getBodyWidth(context) / 3 - 5;
 
     return Container(
-      color: Colors.green,
-      height: 200,
       width: popularItemWidth,
       // 나중에 지웠다 추가했다 해보기
       constraints: BoxConstraints(
@@ -37,18 +36,67 @@ class HomeBodyPopularItem extends StatelessWidget {
   }
 
   Widget _buildPopularItemImage() {
-    return SizedBox();
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset(
+            "assets/${popularList[id]}",
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(height: gap_s),
+      ],
+    );
   }
 
   Widget _buildPopularItemStar() {
-    return SizedBox();
+    return Column(
+      children: [
+        Row(
+          children: [
+            Icon(Icons.star, color: kAccentColor),
+            Icon(Icons.star, color: kAccentColor),
+            Icon(Icons.star, color: kAccentColor),
+            Icon(Icons.star, color: kAccentColor),
+            Icon(Icons.star, color: kAccentColor),
+          ],
+        )
+      ],
+    );
   }
 
   Widget _buildPopularItemComment() {
-    return SizedBox();
+    return Column(
+      children: [
+        Text(
+          "깔끔하고 다 갖춰져있어서 좋았어요:) 위치도 완전 좋아용 다들 여기 살고싶다구ㅋㅋㅋㅋㅋ 화장실도 3개예요!!! 5명이서 씻는것도 전혀 불편함없이 좋았어요^^ 이불도 포근하고 좋습니당ㅎㅎ",
+          style: body1(),
+          maxLines: 3, // 2. 글자 라인을 제한할 수 있다.
+          overflow: TextOverflow.ellipsis, // 3. 글자가 3 라인을 벗어나면 ... 처리된다.
+        ),
+        SizedBox(height: gap_s),
+      ],
+    );
   }
 
   Widget _buildPopularItemUserInfo() {
-    return SizedBox();
+    return Row(
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage("assets/p1.jpeg"),
+        ),
+        SizedBox(width: gap_s),
+        Column(
+          children: [
+            Text(
+              "데어",
+              style: subtitle1(),
+            ),
+            Text("한국"),
+          ],
+        )
+      ],
+    );
   }
 }
